f65b42340ccde5820f820c501835a99899b08a47
Jair <yahir.rod137@gmail.com>
2026-03-05 20:05:19 -0600

    chore:  add data.ts with personal info

---
+++ src/data/data.ts
@@ -0,0 +1,34 @@
+export const data = {
+    name: "Jaime Jair",
+    role: "Full stack Developer",
+    about: "Desarrollador apasionado",
+    social: {
+        github: "https://github.com/yahirjz/",
+        linkedin: "por completar", // <-- falta completar
+        email: "yahir.rod137@gmail.com",
+    },
+    skills: [
+        { name: "React"},
+        { name: "TypeScript" },
+        { name: "Pyhton"},
+        { name: "FataAPI"},
+        { name: "sequelize"},
+        { name: "PostgreSQL"},
+        { name: "GitHub"},
+        { name: "Vercel" },
+        { name: "firebase"},
+        
+    ],
+    projects: [
+        {
+            title: "Mi primer poyecto",
+            description: "falta ", // <-- falta poner la descripción aqui
+            tech: [ "React", "typescript"],
+            github: "#",
+            live: "#"
+        }
+    ]
+       
+
+    
+}
