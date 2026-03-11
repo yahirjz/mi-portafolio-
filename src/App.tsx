47d1a668625b749f77abc11c6d872ead4eb22f4b
Jair <yahir.rod137@gmail.com>
2026-03-11 13:32:03 -0600

    fix: background coverage

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className=" min-h-screen flex flex-col w-full relative justify-center">
+    <div className="min-h-screen flex flex-col w-full" style={{ background: 'radial-gradient(circle at 50% 50%, #1a1040, #0d0d1a)' }}>
         <Header/>
         <Hero/>
         <Projects/>
